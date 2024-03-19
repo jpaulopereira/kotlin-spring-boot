0f6c980f773b81994e3232dd2ebbbb130d0aa853
jpaulopereira <joaopaulo.ae@gmail.com>
2024-03-19 10:00:41 -0300

    mapeamento das entidades JPA

--- src/main/kotlin/br/com/jotape/forum/model/Topico.kt
+++ src/main/kotlin/br/com/jotape/forum/model/Topico.kt
@@ -1,15 +1,28 @@
 package br.com.jotape.forum.model
 
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
+@Entity
 data class Topico (
+
     //val nomeDoAtributo: Tipagem (?)==não obrigatório = valor inicial = null
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     var id: Long? = null,
     val titulo: String,
     val mensagem: String,
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+
+    @ManyToOne //Um tópico pertence a um único curso
     val curso: Curso,
+
+    @ManyToOne
     val autor: Usuario,
+
+    @Enumerated(value = EnumType.STRING)  //salva o nome da constante. Não a ordem Ex: 1,2..
     val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+
+    @OneToMany(mappedBy = "topicos")
     val respostas: List<Resposta> = ArrayList()
 )
